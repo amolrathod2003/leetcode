109220b8441a7f410a0fb6464c0174906b8ccad2
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:42:43 +0530

    Create 904-LeafSimilarTrees

---
+++ 904-LeafSimilarTrees/904-LeafSimilarTrees.java
@@ -0,0 +1,39 @@
+// Last updated: 4/1/2026, 10:42:43 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+        
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+        
+        return leaves1.equals(leaves2);
+    }
+    
+    private void dfs(TreeNode node, List<Integer> leafList) {
+        if (node == null) return;
+        
+        if (node.left == null && node.right == null) {
+            leafList.add(node.val);
+            return;
+        }
+        
+        dfs(node.left, leafList);
+        dfs(node.right, leafList);
+    }
+}
