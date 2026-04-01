8314ab9e9f624688b96cd7b64b031beb528d0dbe
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:43:40 +0530

    Create 541-ReverseStringIi

---
+++ 541-ReverseStringIi/541-ReverseStringIi.java
@@ -0,0 +1,21 @@
+// Last updated: 4/1/2026, 10:43:40 PM
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i += 2 * k) {
+            int start = i;
+            int end = Math.min(i + k - 1, arr.length - 1);
+            reverse(arr, start, end);
+        }
+        return new String(arr);
+    }
+    private void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
