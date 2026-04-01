e974d5757424ef32a296dd1c6052e188d7938609
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:41:46 +0530

    Create 1392-FindTheDifferenceOfTwoArrays

---
+++ 1392-FindTheDifferenceOfTwoArrays/1392-FindTheDifferenceOfTwoArrays.java
@@ -0,0 +1,29 @@
+// Last updated: 4/1/2026, 10:41:45 PM
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+       HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            set2.add(num);
+        }
+        List<Integer> result1 = new ArrayList<>();
+        List<Integer> result2 = new ArrayList<>();
+        for (int num : set1) {
+            if (!set2.contains(num)) {
+                result1.add(num);
+            }
+        }
+        for (int num : set2) {
+            if (!set1.contains(num)) {
+                result2.add(num);
+            }
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(result1);
+        result.add(result2);
+        return result;
+    }
+}
