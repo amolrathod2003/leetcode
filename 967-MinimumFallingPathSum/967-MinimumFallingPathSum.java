7b31e561bbadfd10496280ce640a7b004af8ca04
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:42:29 +0530

    Create 967-MinimumFallingPathSum

---
+++ 967-MinimumFallingPathSum/967-MinimumFallingPathSum.java
@@ -0,0 +1,28 @@
+// Last updated: 4/1/2026, 10:42:28 PM
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        for(int j = 0; j < m; j++){
+            dp[0][j] = matrix[0][j];
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < m; j++){
+                int up = matrix[i][j] + dp[i-1][j];
+                int leftDiag = matrix[i][j];
+                if(j > 0) leftDiag += dp[i-1][j-1];
+                else leftDiag += (int)1e9;
+                int rightDiag = matrix[i][j];
+                if(j < m-1) rightDiag += dp[i-1][j+1];
+                else rightDiag += (int)1e9;
+                dp[i][j] = Math.min(up, Math.min(leftDiag, rightDiag));
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int j = 0; j < m; j++){
+            ans = Math.min(ans, dp[n-1][j]);
+        }
+        return ans;
+    }
+}
