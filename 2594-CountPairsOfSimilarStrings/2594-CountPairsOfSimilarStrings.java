c96faa9956e9077a5a98e9969da9884e3044adfd
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:40:17 +0530

    Create 2594-CountPairsOfSimilarStrings

---
+++ 2594-CountPairsOfSimilarStrings/2594-CountPairsOfSimilarStrings.java
@@ -0,0 +1,28 @@
+// Last updated: 4/1/2026, 10:40:17 PM
+class Solution {
+    public int similarPairs(String[] words) {
+        int count = 0;
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (checkSimilar(words[i], words[j])) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static boolean checkSimilar(String s, String t) {
+        Set<Character> sSet = new HashSet<>();
+        for (char ch : s.toCharArray()) {
+            sSet.add(ch);
+        }
+
+        Set<Character> tSet = new HashSet<>();
+        for (char ch : t.toCharArray()) {
+            tSet.add(ch);
+        }
+
+        return sSet.equals(tSet);
+    }
+}
