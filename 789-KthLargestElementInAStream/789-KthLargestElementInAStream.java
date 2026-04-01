ac81a35882e5676dd177fd874c24471476edeb66
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:43:00 +0530

    Create 789-KthLargestElementInAStream

---
+++ 789-KthLargestElementInAStream/789-KthLargestElementInAStream.java
@@ -0,0 +1,31 @@
+// Last updated: 4/1/2026, 10:43:00 PM
+class KthLargest {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        minHeap=new PriorityQueue<>();
+        for(int n:nums){
+            add(n);
+        }
+        
+    }
+    
+    public int add(int val) {
+        minHeap.offer(val);
+        if(minHeap.size()>k){
+            minHeap.poll();
+        }
+        return minHeap.peek();
+
+        
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
