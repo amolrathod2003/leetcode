2426f963d85b8e99c7164b20dabfd1d63a4a2182
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:42:06 +0530

    Create 1161-ProjectEmployeesI

---
+++ 1161-ProjectEmployeesI/1161-ProjectEmployeesI.sql
@@ -0,0 +1,8 @@
+-- Last updated: 4/1/2026, 10:42:06 PM
+# Write your MySQL query statement below
+SELECT p.project_id, ROUND(AVG(e.experience_years),2) AS average_years
+FROM Project p 
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id
+
