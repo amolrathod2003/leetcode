1246d841390fbba04e593baec47bbb40291ce82c
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:41:29 +0530

    Create 1523-CapitalGainloss

---
+++ 1523-CapitalGainloss/1523-CapitalGainloss.sql
@@ -0,0 +1,7 @@
+-- Last updated: 4/1/2026, 10:41:28 PM
+# Write your MySQL query statement below
+SELECT stock_name, 
+	   SUM(CASE WHEN operation = 'Sell' THEN price
+		   ELSE -price END) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
