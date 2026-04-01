160395d9663f1598991acc14d2a5125d121976f1
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:45:20 +0530

    Create 148-SortList

---
+++ 148-SortList/148-SortList.java
@@ -0,0 +1,40 @@
+// Last updated: 4/1/2026, 10:45:20 PM
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = getMid(head);
+        ListNode right = mid.next;
+        mid.next = null;
+        ListNode leftSorted = sortList(head);
+        ListNode rightSorted = sortList(right);
+        return merge(leftSorted, rightSorted);
+    }
+
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
