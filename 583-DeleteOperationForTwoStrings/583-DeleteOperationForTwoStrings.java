eec79c1aee176f86b516cf23277f9b4fb403f501
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:43:30 +0530

    Create 583-DeleteOperationForTwoStrings

---
+++ 583-DeleteOperationForTwoStrings/583-DeleteOperationForTwoStrings.java
@@ -0,0 +1,22 @@
+// Last updated: 4/1/2026, 10:43:30 PM
+class Solution {
+    public int lcs(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    public int minDistance(String word1, String word2) {
+        return word1.length() + word2.length()-2*lcs(word1,word2);
+        
+    }
+}
