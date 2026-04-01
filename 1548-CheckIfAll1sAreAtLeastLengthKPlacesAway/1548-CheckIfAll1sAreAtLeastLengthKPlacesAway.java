dc02618840f303f32c513acd34fe631498191b1a
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:41:25 +0530

    Create 1548-CheckIfAll1sAreAtLeastLengthKPlacesAway

---
+++ 1548-CheckIfAll1sAreAtLeastLengthKPlacesAway/1548-CheckIfAll1sAreAtLeastLengthKPlacesAway.java
@@ -0,0 +1,16 @@
+// Last updated: 4/1/2026, 10:41:25 PM
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        final int n=nums.length;
+        int last=-200;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                if(i-last-1<k) return false;
+                last=i;
+            }
+        }
+        return true;
+
+        
+    }
+}
