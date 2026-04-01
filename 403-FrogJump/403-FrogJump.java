6a8a69d57a2f84ec0449ecf4a24c75b80c8130bd
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-01 22:44:04 +0530

    Create 403-FrogJump

---
+++ 403-FrogJump/403-FrogJump.java
@@ -0,0 +1,20 @@
+// Last updated: 4/1/2026, 10:44:04 PM
+class Solution {
+    public boolean canCross(int[] stones) {
+        Map<Integer, Set<Integer>> dp = new HashMap<>();
+        for (int stone : stones) {
+            dp.put(stone, new HashSet<>());
+        }
+        dp.get(0).add(0);
+        for (int stone : stones) {
+            for (int jump : dp.get(stone)) {
+                for (int jumpDistance : new int[] {jump - 1, jump, jump + 1}) {
+                    if (jumpDistance > 0 && dp.containsKey(stone + jumpDistance)) {
+                        dp.get(stone + jumpDistance).add(jumpDistance);
+                    }
+                }
+            }
+        }
+        return !dp.get(stones[stones.length - 1]).isEmpty();        
+    }
+}
