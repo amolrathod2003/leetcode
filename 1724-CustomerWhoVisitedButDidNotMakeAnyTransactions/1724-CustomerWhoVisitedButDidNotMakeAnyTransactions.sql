a74b1fe5ab24d5e5765813f1175bd526b5c6f5e8
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-07 19:56:40 +0530

    Create 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions

---
+++ 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions/1724-CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,9 @@
+-- Last updated: 4/7/2026, 7:56:39 PM
+SELECT v.customer_id,
+       COUNT(v.visit_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+       ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id
+ORDER BY v.customer_id;
