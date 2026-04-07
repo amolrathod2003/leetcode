2891178168e690b7502206dceceabd25adaaf10b
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-04-07 19:59:23 +0530

    Create 2110-EmployeesWithMissingInformation

---
+++ 2110-EmployeesWithMissingInformation/2110-EmployeesWithMissingInformation.sql
@@ -0,0 +1,9 @@
+-- Last updated: 4/7/2026, 7:59:22 PM
+SELECT T.employee_id
+FROM  
+  (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+   UNION 
+   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+AS T
+WHERE T.salary IS NULL OR T.name IS NULL
+ORDER BY employee_id;
