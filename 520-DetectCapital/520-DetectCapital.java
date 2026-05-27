8827ef7013d1121cb8821735b70cb5b34dc0a7c1
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-05-27 12:03:55 +0530

    File updated at 5/27/2026, 12:03:55 PM

--- 520-DetectCapital/520-DetectCapital.java
+++ 520-DetectCapital/520-DetectCapital.java
@@ -1,17 +1,6 @@
-// Last updated: 5/27/2026, 11:45:56 AM
+// Last updated: 5/27/2026, 12:03:55 PM
 1class Solution {
 2    public boolean detectCapitalUse(String word) {
-3        if(word.equals(word.toUpperCase())){
-4            return true;
-5        }
-6        if(word.equals(word.toLowerCase())){
-7            return true;
-8        }
-9        String remLet=word.substring(1);
-10        if(Character.isUpperCase(word.charAt(0))&& remLet.equals(remLet.toLowerCase())){
-11            return true;
-12        }
-13        return false;
-14
-15    }
-16}
+3         return word.matches("[A-Z]+|[a-z]+|[A-Z][a-z]*");
+4    }
+5}
