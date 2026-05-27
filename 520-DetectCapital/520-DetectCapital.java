95a47b1d1f555e6959f0a6106b77ade3fbf300a4
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-05-27 12:23:53 +0530

    File updated at 5/27/2026, 12:23:52 PM

--- 520-DetectCapital/520-DetectCapital.java
+++ 520-DetectCapital/520-DetectCapital.java
@@ -1,7 +1,23 @@
-// Last updated: 5/27/2026, 12:16:26 PM
+// Last updated: 5/27/2026, 12:23:52 PM
 1class Solution {
-2    public String toLowerCase(String s) {
-3        return s.toLowerCase();
-4        
-5    }
-6}
+2    public int numberOfSpecialChars(String word) {
+3        boolean[] lowerSeen = new boolean[26];
+4        boolean[] upperSeen = new boolean[26];
+5        for (int i = 0; i < word.length(); i++) {
+6            char ch = word.charAt(i);
+7            if (ch >= 'a' && ch <= 'z') {
+8                lowerSeen[ch - 'a'] = true;
+9            } else if (ch >= 'A' && ch <= 'Z') {
+10                upperSeen[ch - 'A'] = true;
+11            }
+12        }
+13        int specialCount = 0;
+14        for (int i = 0; i < 26; i++) {
+15            if (lowerSeen[i] && upperSeen[i]) {
+16                specialCount++;
+17            }
+18        }
+19        return specialCount;
+20    }
+21}
+22
