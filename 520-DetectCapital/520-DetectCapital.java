309074fa5e49376aa67ee6c2ed7da24d76c50237
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-05-27 12:11:39 +0530

    File updated at 5/27/2026, 12:11:38 PM

--- 520-DetectCapital/520-DetectCapital.java
+++ 520-DetectCapital/520-DetectCapital.java
@@ -1,6 +1,21 @@
-// Last updated: 5/27/2026, 12:03:55 PM
+// Last updated: 5/27/2026, 12:11:38 PM
 1class Solution {
-2    public boolean detectCapitalUse(String word) {
-3         return word.matches("[A-Z]+|[a-z]+|[A-Z][a-z]*");
-4    }
-5}
+2    public String capitalizeTitle(String title) {
+3        String[] words = title.split(" ");
+4        StringBuilder sb = new StringBuilder();
+5        for (int i = 0; i < words.length; i++) {
+6            String word = words[i];
+7            if (word.length() <= 2) {
+8                sb.append(word.toLowerCase());
+9            } else {
+10                sb.append(Character.toUpperCase(word.charAt(0)));
+11                sb.append(word.substring(1).toLowerCase());
+12            }
+13            if (i < words.length - 1) {
+14                sb.append(" ");
+15            }
+16        }
+17        return sb.toString();
+18    }
+19}
+20
