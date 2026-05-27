99bde61b60536f22f95dd2ee4279f404ab142fcc
amolrathod2003 <khtravathamo143256789@gmail.com>
2026-05-27 12:16:26 +0530

    File updated at 5/27/2026, 12:16:26 PM

--- 520-DetectCapital/520-DetectCapital.java
+++ 520-DetectCapital/520-DetectCapital.java
@@ -1,21 +1,7 @@
-// Last updated: 5/27/2026, 12:11:38 PM
+// Last updated: 5/27/2026, 12:16:26 PM
 1class Solution {
-2    public String capitalizeTitle(String title) {
-3        String[] words = title.split(" ");
-4        StringBuilder sb = new StringBuilder();
-5        for (int i = 0; i < words.length; i++) {
-6            String word = words[i];
-7            if (word.length() <= 2) {
-8                sb.append(word.toLowerCase());
-9            } else {
-10                sb.append(Character.toUpperCase(word.charAt(0)));
-11                sb.append(word.substring(1).toLowerCase());
-12            }
-13            if (i < words.length - 1) {
-14                sb.append(" ");
-15            }
-16        }
-17        return sb.toString();
-18    }
-19}
-20
+2    public String toLowerCase(String s) {
+3        return s.toLowerCase();
+4        
+5    }
+6}
